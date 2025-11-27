a6a9537b3fb04efd8cf20ad1cb70605dee4937ab
Ayham-A4M <ayham10.aboajeb@gmail.com>
2025-11-27 18:04:29 +0300

    make pdf responsive

---
+++ src/components/pdf-viewr.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useRef, useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import { Button } from "./ui/button";
+import Spinner from "./ui/spinner";
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+const PdfViewr = ({ pdfUrl }: { pdfUrl: string }) => {
+  const [numberOfPage, setNumberOfPage] = useState<number>(1);
+  const [numPages, setNumPages] = useState<number>(1);
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+    console.log(numPages);
+    setNumPages(numPages);
+    setNumberOfPage(1);
+  }
+  const [windowSize, setWindowSize] = useState<number>(window?.innerWidth);
+  const parentRef = useRef<null | HTMLDivElement>(null);
+  const [parentWidth, setParentWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => setWindowSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (parentRef.current) {
+      setParentWidth(parentRef.current.offsetWidth);
+    }
+  }, [windowSize]);
+  return (
+    <div ref={parentRef} className="w-full">
+      <Document file={pdfUrl ? pdfUrl : ""} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page
+          loading={
+            <div className="w-full h-full flex items-center justify-center">
+              <Spinner />
+            </div>
+          }
+          pageNumber={numberOfPage}
+          width={parentWidth}
+          renderTextLayer={false}
+          renderAnnotationLayer={false}
+        />
+      </Document>
+      <div className="flex py-6 items-center gap-6">
+        <Button
+          disabled={numberOfPage === 1}
+          className="text-slate-200"
+          onClick={() => {
+            if (numberOfPage > 1) setNumberOfPage((prev) => prev - 1);
+          }}
+        >
+          Prev
+        </Button>
+        <Button
+          disabled={numberOfPage == numPages}
+          className="text-slate-200"
+          onClick={() => {
+            console.log(numberOfPage + 1, "number of paaaaage");
+            if (numberOfPage<numPages) setNumberOfPage((prev) => prev + 1);
+          }}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PdfViewr;
--- src/pages/instructor-pages/edit-lessson/edit-lesson.tsx
+++ src/pages/instructor-pages/edit-lessson/edit-lesson.tsx
@@ -7,24 +7,16 @@ import { Button } from "@/components/ui/button";
 import usePUT from "@/hooks/usePUT";
 import Spinner from "@/components/ui/spinner";
 import LessonInformation from "@/components/shared/lesson-information";
-import { FaFileAlt } from "react-icons/fa";
-import { FaCircleMinus } from "react-icons/fa6";
+import PdfViewr from "@/components/pdf-viewr";
 
-import { Document, Page } from "react-pdf";
-import { pdfjs } from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 const EditLesson = () => {
   const { lessonId, courseId } = useParams();
   const { data, isLoading } = useGET(`/api/instructor/courses/${courseId}/lessons/${lessonId}`, []);
   const [jsonState, setJsonState] = useState<string>("");
   const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<null | string>(null);
   const formDataRef = useRef<FormData>(new FormData());
-  const { mutation } = usePUT(
-    `/api/instructor/courses/${courseId}/lessons/${lessonId}`,
-    formDataRef.current,
-    ["lesson"]
-  );
+  const { mutation } = usePUT(`/api/instructor/courses/${courseId}/lessons/${lessonId}`, formDataRef.current, ["lesson"]);
   useEffect(() => {
     setJsonState("");
     if (data?.lesson?.lessonContent) {
@@ -36,7 +28,9 @@ const EditLesson = () => {
       return;
     }
     const file = e?.target?.files[0];
+    const fileUrl = URL.createObjectURL(file);
     setFile(file);
+    setFileUrl(fileUrl);
   };
 
   const handleSaveChanges = () => {
@@ -48,7 +42,7 @@ const EditLesson = () => {
     formDataRef.current = formData;
     mutation?.mutate();
   };
-const [numberOfPage,setNumberOfPage]=useState<number>(1)
+
   return (
     <>
       {isLoading ? (
@@ -63,42 +57,29 @@ const [numberOfPage,setNumberOfPage]=useState<number>(1)
                 description: data?.lesson?.description,
               }}
             />
+            <h1 className="text-2xl text-primary  flex items-center gap-3">
+              <span className="rounded-full bg-primary size-8 flex items-center justify-center text-[1rem] text-slate-200">1</span>
+              Select pdf file
+            </h1>
 
             <input type="file" accept=".pdf" className="hidden" id="input-file" onChange={handleChooseFile} />
-            <label
-              htmlFor="input-file"
-              className="px-6 py-2 bg-primary text-slate-200 rounded-[2px] cursor-pointer"
-            >
-              {file || data?.lesson?.pdfUrl ? "change file" : "choose file"}
-            </label>
-            {file ? (
-              <div className="pt-6 flex items-center">
-                <div className="flex w-fit justify-center text-primary flex-col gap-2">
-                  <FaFileAlt className="size-14" />
-                  <span className="max-w-[50%] text-[.8rem]">
-                    {file?.name.slice(0, 10)}
-                    {file?.name?.length > 10 ? "...(.pdf)" : ""}
-                  </span>
-                </div>
-                <button
-                  className="w-fit cursor-pointer"
-                  name="discard-file"
+            <div className="flex items-center gap-6">
+              <label htmlFor="input-file" className="px-6 py-2 bg-primary text-slate-200 rounded-[2px] cursor-pointer">
+                {file || data?.lesson?.pdfUrl ? "change file" : "choose file"}
+              </label>
+              {file && (
+                <Button
+                  className="bg-red-500 text-slate-200"
                   onClick={() => {
                     setFile(null);
+                    setFileUrl(null);
                   }}
                 >
-                  <FaCircleMinus className="text-2xl text-red-500" />
-                </button>
-              </div>
-            ) : (
-              data?.lesson?.pdfUrl && (
-                <div className="pt-6">
-                  <div className="flex w-fit justify-center text-primary flex-col gap-2">
-                    <FaFileAlt className="size-14" />
-                  </div>
-                </div>
-              )
-            )}
+                  Discard
+                </Button>
+              )}
+            </div>
+
             {data?.lesson?.lessonContent !== jsonState && (
               <div className="flex items-center justify-end">
                 <Button
@@ -112,25 +93,22 @@ const [numberOfPage,setNumberOfPage]=useState<number>(1)
               </div>
             )}
           </div>
-          <div className="w-full overflow-hidden">
-            <Document file={`http://localhost:8000${data?.lesson?.pdfUrl}`}>
-              <Page pageNumber={numberOfPage} renderTextLayer={false} renderAnnotationLayer={false}/>
-            </Document>
-            <button onClick={()=>{setNumberOfPage(prev=>prev+1)}}>inc</button>
-          </div>
 
-          {data?.lesson && (
-            <LexicalEditor key={data?.lesson?._id} jsonState={jsonState} onChange={setJsonState} />
-          )}
+          <PdfViewr pdfUrl={fileUrl ? fileUrl : `http://localhost:8000${data?.lesson?.pdfUrl}`} />
+
+          <h1 className="text-2xl text-primary  flex items-center gap-3">
+            <span className="rounded-full bg-primary size-8 flex items-center justify-center text-[1rem] text-slate-200 ">2</span>
+            Notes and important things
+          </h1>
+
+          {data?.lesson && <LexicalEditor key={data?.lesson?._id} jsonState={jsonState} onChange={setJsonState} />}
           <div className="flex justify-end">
             <div className="flex justify-end items-center">
               <Button
                 variant={"outline"}
                 className="cursor-pointer border-3 border-primary text-primary rounded-xl font-semibold hover:bg-primary hover:text-slate-100 "
               >
-                <Link to={`/instructor/courses/${courseId}/lessons/${lessonId}/edit/quizzes`}>
-                  Management Realated Questions
-                </Link>
+                <Link to={`/instructor/courses/${courseId}/lessons/${lessonId}/edit/quizzes`}>Management Realated Questions</Link>
               </Button>
             </div>
           </div>
